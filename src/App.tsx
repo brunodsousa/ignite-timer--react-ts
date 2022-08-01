ec1db74c751717a5d59a5977f4113003620e3ef0
Bruno Sousa <brunodsousa@outlook.com>
2022-07-31 22:20:40 -0300

    organizando importações

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
-import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
